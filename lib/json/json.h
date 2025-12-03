8ec392b888463a4813d8b910ba834130b104eb96
Alex Veden (streamer) <streamer@alexveden.com>
2025-12-03 12:08:47 +0400

    json docs

--- lib/json/json.h
+++ lib/json/json.h
@@ -4,16 +4,35 @@
 #    define CEX_MAX_JSON_DEPTH 128
 #endif
 
+/// JSON Reader Namespace
+#define __jr$
+
+/// Creates new json reader container, kwargs... are optional see: json_reader_kw. json_reader is a
+/// one pass, non-allocating parser.
 #define jr$new(json_reader, content, len, kwargs...)                                               \
     _cex_json__reader__create((json_reader), (content), (len), &(json_reader_kw){ kwargs })
 
+/// Gets last json reader error
 #define jr$err(json_reader) (json_reader)->error
 
+/*clang-format off*/
+
+/// Returns ("format", err, args) text macro compatible with any printf() functions, used for
+/// printing/formatting JSON debug message with reference to error type and line/col position of an
+/// error.
+/// Examples:
+/// str.fmt(alloc, jr$err_fmt(&json_reader));
+/// io.printf(jr$err_fmt(&json_reader));
+/// io.fprintf(stderr, jr$err_fmt(&json_reader));
 #define jr$err_fmt(json_reader)                                                                    \
     "JSON %s(%s) at line: %d col: %d\n", (json_reader)->error ? "Parsing Error " : "",             \
         (json_reader)->error ? (json_reader)->error : "OK", (json_reader)->_impl.lexer.line + 1,   \
         (json_reader)->_impl.lexer.col
 
+/*clang-format on*/
+
+/// Executes (try_expression) (must return Exc or Exception type), and sets last json_reader.error
+/// on fail, after this does `goto goto_on_fail_label`. Prints tracebacks in unit test suites.
 #define jr$egoto(json_reader, try_expression, goto_on_fail_label)                                  \
     if (!(json_reader)->error) {                                                                   \
         e$except_silent (err, try_expression) {                                                    \
@@ -22,8 +41,15 @@
         }                                                                                          \
     }
 
-#define jr$get_scope_str_s(json_reader, json_type) _cex_json__reader__get_scope((json_reader), json_type)
+/// Gets next `json_type` scope as a string slice (str_s), you may use another json_reade instance
+/// to parse result of this function. Sets json_reader.error + returns (str_s){0} on failure.
+#define jr$get_scope_str_s(json_reader, json_type)                                                 \
+    _cex_json__reader__get_scope((json_reader), json_type)
 
+/// Context specific iterator over json_reader scope:
+/// jr$foreach(val, json_reader) - iterates over array scope items
+/// jr$foreach(key, val, json_reader) - iterates over object scope key:value pairs
+/// key and val - are typeof(str_s)
 #define jr$foreach(...) _jr$foreach_impl(__VA_ARGS__, _jr$foreach_obj, _jr$foreach_arr)(__VA_ARGS__)
 
 #define _jr$foreach_impl(_1, _2, _3, NAME, ...) NAME
@@ -107,13 +133,22 @@ typedef struct json_writer_c
     u8 scope_stack[CEX_MAX_JSON_DEPTH];
 } json_writer_c;
 
+/// JSON Writer Namespace
+#define __jw$
+
+/// Creates new instance of json writer, non allocating serializer, with support of exporting to
+/// FILE* or backing by string buffer sbuf_c
 #define jw$new(json_writer, kwargs...)                                                             \
     _cex_json__writer__create((json_writer), &(json_writer_kw){ kwargs })
 
+/// Checks if json writer has no errors
 #define jw$validate(json_writer) _cex_json__writer__validate((json_writer))
 
-#define jw$key(format, ...) _cex_json__writer__print_key(jw$scope_var, format, ##__VA_ARGS__)
+/// Writes a new key (must be in jw$scope(jw, JsonType__obj))
+#define jw$key(format, ...) _cex_json__writer__print_key(_jw$scope_var, format, ##__VA_ARGS__)
 
+/// Writes a new value to the json scope (object or array), expects json compatible primitive
+/// arguments. Use `jr$fmt` for customizable output.
 #define jw$val(json_compatible_val)                                                                \
     ({                                                                                             \
         char* format = _Generic(                                                                   \
@@ -134,23 +169,23 @@ typedef struct json_writer_c
             const char*: "\"%s\"",                                                                 \
             char*: "\"%s\""                                                                        \
         );                                                                                         \
-        _cex_json__writer__print_item(jw$scope_var, format, json_compatible_val);                  \
+        _cex_json__writer__print_item(_jw$scope_var, format, json_compatible_val);                  \
     })
 
-#define jw$scope_var _json_writer_macro_scope
+#define _jw$scope_var _json_writer_macro_scope
 
-/// Opens JSON buffer scope (json_writer_ptr data is cleared out)
+/// Opens JSON scope, jsontype_arr_or_obj expects JsonType__obj or JsonType__arr
 #define jw$scope(json_writer_ptr, jsontype_arr_or_obj)                                             \
-    for (json_writer_c * jw$scope_var                                                              \
+    for (json_writer_c * _jw$scope_var                                                              \
              __attribute__((__cleanup__(_cex_json__writer__print_scope_exit))) =                   \
              _cex_json__writer__print_scope_enter((json_writer_ptr), jsontype_arr_or_obj, true),   \
-             *cex$tmpname(jsonbuf_sentinel) = jw$scope_var;                                        \
-         cex$tmpname(jsonbuf_sentinel) && jw$scope_var != NULL;                                    \
+             *cex$tmpname(jsonbuf_sentinel) = _jw$scope_var;                                        \
+         cex$tmpname(jsonbuf_sentinel) && _jw$scope_var != NULL;                                    \
          cex$tmpname(jsonbuf_sentinel) = NULL)
 
 
-/// Append any formatted string, it's for low level printing (jw$scope)
-#define jw$fmt(format, ...) _cex_json__writer__print(jw$scope_var, format, ##__VA_ARGS__)
+/// Append any formatted string in the jw$scope, it's for low level printing
+#define jw$fmt(format, ...) _cex_json__writer__print(_jw$scope_var, format, ##__VA_ARGS__)
 
 // clang-format off
 Exception _cex_json__reader__create(json_reader_c* it, char* content, usize content_len, json_reader_kw* kwargs);
