66edb22588c71cb37d2462e0153f87a54d207bc7
Alex Veden <i@alexveden.com>
2026-06-05 12:13:25 +0400

    example: fixed semicol

--- examples/freestanding/src/platform_os.c
+++ examples/freestanding/src/platform_os.c
@@ -151,7 +151,7 @@ clock_gettime(clockid_t clockid, struct timespec* tp)
     return 0;
 }
 
-int nanosleep(const struct timespec *req, struct timespec *rem);
+int nanosleep(const struct timespec *req, struct timespec *rem)
 {
     return 0;
 }
