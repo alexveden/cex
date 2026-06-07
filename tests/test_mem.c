9136224386047680ab88d8fd6b47a020d4d75ba7
Alex Veden <i@alexveden.com>
2026-06-07 14:58:03 +0400

    test: mem$..._overflow checks

--- tests/test_mem.c
+++ tests/test_mem.c
@@ -45,4 +45,50 @@ test$case(test_aligned_size)
     return EOK;
 }
 
+test$case(test_mem_add_overflow)
+{
+    usize r;
+    tassert(!mem$add_overflow((usize)10, (usize)20, &r));
+    tassert_eq(r, 30);
+    tassert(mem$add_overflow((usize)-1, (usize)1, &r));
+    tassert_eq(r, 0);
+
+    isize sr;
+    tassert(mem$add_overflow((isize)9223372036854775807, (isize)1, &sr));
+
+    u32 ur;
+    tassert(!mem$add_overflow((u32)4000000000, (u32)1, &ur));
+    tassert_eq(ur, (u32)4000000001);
+    return EOK;
+}
+
+test$case(test_mem_sub_overflow)
+{
+    usize r;
+    tassert(!mem$sub_overflow((usize)20, (usize)10, &r));
+    tassert_eq(r, 10);
+    tassert(mem$sub_overflow((usize)0, (usize)1, &r));
+    tassert_eq(r, (usize)-1);
+
+    isize sr;
+    tassert(mem$sub_overflow((isize)(-9223372036854775807 - 1), (isize)1, &sr));
+    return EOK;
+}
+
+test$case(test_mem_mul_overflow)
+{
+    usize r;
+    tassert(!mem$mul_overflow((usize)100, (usize)200, &r));
+    tassert_eq(r, 20000);
+    tassert(mem$mul_overflow((usize)-1, (usize)2, &r));
+    tassert(!mem$mul_overflow((usize)((usize)-1 / 2), (usize)2, &r));
+    tassert(mem$mul_overflow((usize)((usize)-1 / 2 + 1), (usize)2, &r));
+
+    u32 ur;
+    tassert(!mem$mul_overflow((u32)50000, (u32)50000, &ur));
+    tassert_eq(ur, (u32)2500000000);
+    tassert(mem$mul_overflow((u32)100000, (u32)100000, &ur));
+    return EOK;
+}
+
 test$main();
